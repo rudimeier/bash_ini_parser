0eb9069e35c0aeb71bf5b62ea7b79867575be419
jcass <john.cass77@gmail.com>
2015-04-19 17:48:04 +0200

    Fix --allow_no_value to act as toggle instead of accepting value.

--- test/test12.sh
+++ test/test12.sh
@@ -7,7 +7,7 @@
 
 unset INI
 declare -A INI
-read_ini --booleans 0 --allow_no_value 1 test12.ini
+read_ini --booleans 0 --allow_no_value test12.ini
 
 echo "No Values_key_without_value:${INI[No Values__key_without_value]}"
 echo "No Values_empty string value here:${INI[No Values__empty string value here]}"
